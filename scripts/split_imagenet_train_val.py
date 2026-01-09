5f0bd822131f434af7b9ad0495c4dccdb1507cda
Sachin Kelkar <sachinkel19@gmail.com>
2026-01-08 20:51:36 -0800

    bugfix in split imagenet script

--- scripts/split_imagenet_train_val.py
+++ scripts/split_imagenet_train_val.py
@@ -14,12 +14,32 @@ num_val_samples = 5000
 num_samples = images.shape[0]
 num_train_samples = num_samples - num_val_samples
 
-train_images = images[:num_train_samples]
-np.save(data_path.with_suffix(".train.npy"), train_images)
-with open(labels_path.with_suffix(".train.json"), "w") as f:
-    json.dump(labels[:num_train_samples], f)
+# Shuffle indices with fixed seed for reproducibility
+rng = np.random.default_rng(seed=42)
+indices = rng.permutation(num_samples)
+train_indices = indices[:num_train_samples]
+val_indices = indices[num_train_samples:]
 
-val_images = images[num_train_samples:]
-np.save(data_path.with_suffix(".val.npy"), val_images)
+train_out = np.memmap(
+    data_path.with_suffix(".train.npy"),
+    dtype="uint8",
+    mode="w+",
+    shape=(num_train_samples, 4096)
+)
+train_out[:] = images[train_indices]
+train_out.flush()
+del train_out
+with open(labels_path.with_suffix(".train.json"), "w") as f:
+    json.dump([labels[i] for i in train_indices], f)
+
+val_out = np.memmap(
+    data_path.with_suffix(".val.npy"),
+    dtype="uint8",
+    mode="w+",
+    shape=(num_val_samples, 4096)
+)
+val_out[:] = images[val_indices]
+val_out.flush()
+del val_out
 with open(labels_path.with_suffix(".val.json"), "w") as f:
-    json.dump(labels[num_train_samples:], f)
+    json.dump([labels[i] for i in val_indices], f)
